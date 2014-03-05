0993a8371e005c2a92525b449733cbd447de8954
PuloV <yordanpulov@gmail.com>
2014-03-05 23:10:13 +0200

    Add index-by function

--- Homework1/solution.clj
+++ Homework1/solution.clj
@@ -4,8 +4,6 @@
           (rem n 10))
    [n])
   )
-;;Problem s nachalo s 0
-;;(digits 136)
 
 (defn prime-factors [n]
   (letfn [(prime [number]
@@ -18,14 +16,22 @@
 
           (join-prime-factors [num]
             (letfn [(helper [numb iter]
-             (cond
-                 (and (prime iter) ( = (rem numb iter) 0 )) (conj (helper (quot numb iter) iter) iter)
-                 (< iter numb) (helper numb (inc iter))
-                 (= iter numb) [numb]
+             (cond (and (prime iter) ( = (rem numb iter) 0 )) (conj (helper (quot numb iter) iter) iter)
+                   (< iter numb) (helper numb (inc iter))
+                   (= iter numb) [numb]
              ))]
               (helper num 2) )) ]
 
     (if (or (= n 1) (= n 0)) [] (join-prime-factors n) )
     ))
 
+(defn fizbuz [to-num]
+  (cond (= (rem to-num 15) 0) (conj (fizbuz (dec to-num)) "fizzbuzz")
+        (= (rem to-num 5) 0) (conj (fizbuz (dec to-num)) "buzz")
+        (= (rem to-num 3) 0) (conj (fizbuz (dec to-num)) "fizz")
+        (= to-num 1) [to-num]
+        :else (conj (fizbuz (dec to-num)) to-num)
+        ))
 
+(defn index-by [func l]
+  (if-not (empty? l) (conj (index-by func (pop l)) {(func(last l))  (last l)})   {}))
