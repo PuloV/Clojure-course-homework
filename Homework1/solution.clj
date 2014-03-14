2e09afeac51e3c1e5516299034bfd697c7d184c2
PuloV <yordanpulov@gmail.com>
2014-03-14 12:22:38 +0200

    My first ruby homework

--- Homework1/solution.clj
+++ Homework1/solution.clj
@@ -25,13 +25,43 @@
     (if (or (= n 1) (= n 0)) [] (join-prime-factors n) )
     ))
 
-(defn fizbuz [to-num]
-  (cond (= (rem to-num 15) 0) (conj (fizbuz (dec to-num)) "fizzbuzz")
-        (= (rem to-num 5) 0) (conj (fizbuz (dec to-num)) "buzz")
-        (= (rem to-num 3) 0) (conj (fizbuz (dec to-num)) "fizz")
+(defn fizzbuzz [to-num]
+  (cond (= (rem to-num 15) 0) (conj (fizzbuzz (dec to-num)) "fizzbuzz")
+        (= (rem to-num 5) 0) (conj (fizzbuzz (dec to-num)) "buzz")
+        (= (rem to-num 3) 0) (conj (fizzbuzz (dec to-num)) "fizz")
         (= to-num 1) [to-num]
-        :else (conj (fizbuz (dec to-num)) to-num)
+        :else (conj (fizzbuzz (dec to-num)) to-num)
         ))
 
 (defn index-by [func l]
   (if-not (empty? l) (conj (index-by func (pop l)) {(func(last l))  (last l)})   {}))
+
+(defn harmonic [n]
+  (if (= n 1) 1 (+ (/ 1 n) (harmonic (dec n))) ))
+
+(defn densities [list]
+  (letfn [( counts-accur[current-list , item-to-be-found ,acur]
+            (cond (empty? current-list) acur
+                  (= item-to-be-found (last current-list)) (counts-accur (pop current-list) item-to-be-found (inc acur))
+                  :else (counts-accur (pop current-list) item-to-be-found acur)))
+          (to-keywords [old-list , new-list]
+            (if (empty? old-list) new-list
+            (to-keywords (pop old-list) (conj new-list (counts-accur list (last old-list) 0)))))]
+  (reverse (to-keywords list [] ))))
+
+(defn uniquify [not-uniq-list]
+  (letfn [(find-uniq[element , occurred , uniq-list]
+                    (cond (not (some #(= element %) uniq-list)) element
+                          (= occurred 0) (find-uniq (str element "-" (inc occurred)) (inc occurred) uniq-list)
+                          :else (find-uniq (clojure.string/replace element (str "-" occurred ) (str "-" (inc occurred) )) (inc occurred) uniq-list))
+                          )
+
+          (make-uniq-list [not-uniq , uniq]
+                 (cond (empty? not-uniq) uniq
+                       :else  (make-uniq-list (rest not-uniq) (conj uniq (find-uniq (first not-uniq) 0 uniq)))
+                  )
+           )]
+    (make-uniq-list not-uniq-list [])))
+
+
+
